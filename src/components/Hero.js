447f2290c72eb0e79e338cf2ed004ce34b12a2f1
Divyanshi Naithani <naithani1604@gmail.com>
2025-01-21 10:52:21 +0530

    leetcode profile add

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -16,7 +16,7 @@ const Hero = () => {
 
   // Function to handle resume download
   const handleResumeDownload = () => {
-    const resumeUrl = "./resume DivyanshiNaithani.pdf "; 
+    const resumeUrl = "./resume DivyanshiNaithani.pdf"; 
     const link = document.createElement("a");
     link.href = resumeUrl;
     link.download = "Divyanshi_Naithani_Resume.pdf"; 
@@ -25,98 +25,114 @@ const Hero = () => {
     document.body.removeChild(link);
   };
 
-
   return (
     <section id="hero" className="hero-section">
-    <main className="flex flex-row items-center justify-center mt-10 px-5">
-      <div className="flex flex-col items-center mr-4">
-        {/* Social Media Icons */}
-        <div className="flex flex-col gap-4">
-          {/* GitHub */}
-          <a
-            href="https://github.com/DivNai"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="group"
-          >
-            <div className="text-gray-400 group-hover:text-pink-500 transition">
-              <i className="fab fa-github fa-lg"></i>
-            </div>
-          </a>
+      <main className="flex flex-row items-center justify-center mt-10 px-5">
+        <div className="flex flex-col items-center mr-4">
+          {/* Social Media Icons */}
+          <div className="flex flex-col gap-4">
+            {/* GitHub */}
+            <a
+              href="https://github.com/DivNai"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="group"
+            >
+              <div className="text-gray-400 group-hover:text-pink-500 transition">
+                <i className="fab fa-github fa-lg"></i>
+              </div>
+            </a>
 
-          {/* LinkedIn */}
-          <a
-            href="https://www.linkedin.com/in/divyanshi-naithani?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="group"
-          >
-            <div className="text-gray-400 group-hover:text-pink-500 transition">
-              <i className="fab fa-linkedin fa-lg"></i>
-            </div>
-          </a>
+            {/* LinkedIn */}
+            <a
+              href="https://www.linkedin.com/in/divyanshi-naithani?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="group"
+            >
+              <div className="text-gray-400 group-hover:text-pink-500 transition">
+                <i className="fab fa-linkedin fa-lg"></i>
+              </div>
+            </a>
 
-          {/* Twitter */}
-          {/* <a
-            href="https://twitter.com/"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="group"
-          >
-            <div className="text-gray-400 group-hover:text-pink-500 transition">
-              <i className="fab fa-twitter fa-lg"></i>
-            </div>
-          </a> */}
+            {/* LeetCode
+            <a
+              href="https://leetcode.com/Div_Nai/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="group"
+            >
+              <div className="text-gray-400 group-hover:text-pink-500 transition">
+                <i className="fab fa-leetcode fa-lg"></i>
+              </div>
+            </a> */}
 
-          {/* Email */}
-          <a
-            href="naithani1604@gmail.com"
-            className="group"
-          >
-            <div className="text-gray-400 group-hover:text-pink-500 transition">
-              <i className="fas fa-envelope fa-lg"></i>
-            </div>
-          </a>
+              <a
+              href="https://leetcode.com/Div_Nai/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="group"
+            >
+              <div className="text-gray-400 group-hover:text-pink-500 transition">
+                <img 
+                  src="./LeetCode_avatar.png" 
+                  alt="Leetcode LOGO" 
+                  className="w-6 h-6" 
+                />
+              </div>
+            </a>
+            
+
+            {/* Email */}
+            <a
+              href="mailto:naithani1604@gmail.com"
+              className="group"
+            >
+              <div className="text-gray-400 group-hover:text-pink-500 transition">
+                <i className="fas fa-envelope fa-lg"></i>
+              </div>
+            </a>
+          </div>
         </div>
-      </div>
 
-      {/* Vertical Line */}
-      <div className="w-1 h-32 bg-gradient-to-r from-pink-500 to-orange-500 rounded-full mx-4"></div>
+        {/* Vertical Line */}
+        <div className="w-1 h-32 bg-gradient-to-r from-pink-500 to-orange-500 rounded-full mx-4"></div>
 
-      {/* Right Section: Profile Picture, Title, Description, and Buttons */}
-      <div className="flex flex-col items-center text-center">
-        <img
-          src="./141929578.png"
-          alt="Profile"
-          className="rounded-full w-40 h-40 border-4 border-gradient-to-r from-pink-500 to-orange-500"
-        />
+        {/* Right Section: Profile Picture, Title, Description, and Buttons */}
+        <div className="flex flex-col items-center text-center">
+          <img
+            src="./141929578.png"
+            alt="Profile"
+            className="rounded-full w-40 h-40 border-4 border-gradient-to-r from-pink-500 to-orange-500"
+          />
 
-        {/* Title with Typewriter Effect */}
-        <h1 className="mt-5 text-4xl font-bold">
-          <span className="text-transparent bg-clip-text bg-gradient-to-r from-pink-500 to-orange-500">
-            {text}
-          </span>
-          <span className="blinking-cursor">|</span> {/* Blinking cursor */}
-        </h1>
+          {/* Title with Typewriter Effect */}
+          <h1 className="mt-5 text-4xl font-bold">
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-pink-500 to-orange-500">
+              {text}
+            </span>
+            <span className="blinking-cursor">|</span> {/* Blinking cursor */}
+          </h1>
 
-        {/* Description */}
-        <p className="mt-4 text-lg max-w-3xl">
-          I'm Divyanshi, a dedicated frontend developer with extensive experience in creating numerous websites. I am passionate about exploring and mastering new technologies to enhance my skills and stay ahead in the ever-evolving tech landscape.
-        </p>
+          {/* Description */}
+          <p className="mt-4 text-lg max-w-3xl">
+            I'm Divyanshi, a dedicated frontend developer with extensive experience in creating numerous websites. I am passionate about exploring and mastering new technologies to enhance my skills and stay ahead in the ever-evolving tech landscape.
+          </p>
 
-        {/* Buttons */}
-        <div className="mt-6 flex space-x-4">
-          <button 
-            onClick={handleResumeDownload} 
-            className="border-2 border-white px-6 py-2 rounded-lg hover:bg-white hover:text-black"
-          >
-            My Resume
-          </button>
+          {/* Buttons */}
+          <div className="mt-6 flex space-x-4">
+            <button 
+              onClick={handleResumeDownload} 
+              className="border-2 border-white px-6 py-2 rounded-lg hover:bg-white hover:text-black"
+            >
+              My Resume
+            </button>
+          </div>
         </div>
-      </div>
-    </main>
+      </main>
     </section>
   );
 };
 
 export default Hero;
+
